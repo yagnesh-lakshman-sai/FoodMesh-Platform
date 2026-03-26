a90deb09005a7ca96781cc24514d548ebab29a4b
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-03-26 22:33:46 +0530

    Intiliaze the RestaurantManagmentApplication project

---
+++ RestaurantManagment/src/main/java/com/foods/RestaurantManagmentApplication.java
@@ -0,0 +1,14 @@
+package com.flmfoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagmentApplication.class, args);
+	}
+
+}
+
---
+++ RestaurantManagment/src/test/java/com/flmfoods/RestaurantManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flmfoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
