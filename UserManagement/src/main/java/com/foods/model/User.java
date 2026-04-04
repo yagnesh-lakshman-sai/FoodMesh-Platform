8fa407a2a6de54e6461c75231381b2c59cee3eb6
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-04-04 21:53:11 +0530

    add User Class

---
+++ UserManagement/src/main/java/com/foods/model/User.java
@@ -0,0 +1,36 @@
+package com.flmfoods.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+
+    private String userName;
+    private String phoneNum;
+    private String email;
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<UserAddress> userAddress;
+}
+
